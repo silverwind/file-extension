493c89d0cad86934a51f088defa37bde21cc62e1
silverwind <me@silverwind.io>
2015-08-07 23:21:40 +0200

    Update all the things, reindent

--- file-extension.js
+++ file-extension.js
@@ -1,37 +1,37 @@
 "use strict";
 
 function ext(filename, opts) {
-    var ret;
+  var ret;
 
-    if (!opts) {
-        opts = {};
-    }
+  if (!opts) {
+    opts = {};
+  }
 
-    if (!filename) {
-        return "";
-    }
+  if (!filename) {
+    return "";
+  }
 
-    if (/^\..+$/.test(filename) && filename.match(/\./g).length === 1) {
-        ret = filename.substring(1);
+  if (/^\..+$/.test(filename) && filename.match(/\./g).length === 1) {
+    ret = filename.substring(1);
+  } else {
+    var parts = filename.split(".");
+
+    if (parts.length === 1 || (parts[0] === "" && parts.length === 2)) {
+      return "";
     } else {
-        var parts = filename.split(".");
-
-        if (parts.length === 1 || (parts[0] === "" && parts.length === 2)) {
-            return "";
-        } else {
-            ret = parts.pop();
-        }
+      ret = parts.pop();
     }
+  }
 
-    return opts.preserveCase ? ret : ret.toLowerCase();
+  return opts.preserveCase ? ret : ret.toLowerCase();
 }
 
 (function (mod) {
-    if (typeof exports === "object") {
-        module.exports = mod();
-    } else if (typeof define === "function" && define.amd) {
-        define([], mod);
-    } else {
-        this.ext = mod();
-    }
+  if (typeof exports === "object") {
+    module.exports = mod();
+  } else if (typeof define === "function" && define.amd) {
+    define([], mod);
+  } else {
+    this.ext = mod();
+  }
 }(function () { return ext; }));
--- test.js
+++ test.js
@@ -5,13 +5,13 @@ var assert = require("assert");
 var ext = require("./");
 
 it("should return correct extensions", function () {
-    assert(ext("") === "");
-    assert(ext(".") === "");
-    assert(ext("file.") === "");
-    assert(ext(".a.js") === "js");
-    assert(ext("file.zip") === "zip");
-    assert(ext("NoExtension") === "");
-    assert(ext(".Dockerfile") === "dockerfile");
-    assert(ext("INDEX.HTML", {preserveCase: true}) === "HTML");
-    assert(ext(".Dockerfile", {preserveCase: true}) === "Dockerfile");
+  assert(ext("") === "");
+  assert(ext(".") === "");
+  assert(ext("file.") === "");
+  assert(ext(".a.js") === "js");
+  assert(ext("file.zip") === "zip");
+  assert(ext("NoExtension") === "");
+  assert(ext(".Dockerfile") === "dockerfile");
+  assert(ext("INDEX.HTML", {preserveCase: true}) === "HTML");
+  assert(ext(".Dockerfile", {preserveCase: true}) === "Dockerfile");
 });
